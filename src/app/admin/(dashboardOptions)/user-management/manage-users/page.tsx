96c2bdff6f6662f23e79a361484703c3e6fa54e3
Omobayode6 <omobayode93@gmail.com>
2024-03-23 14:00:04 +0100

    Add user management cards

--- src/app/admin/(dashboardOptions)/user-management/manage-users/page.tsx
+++ src/app/admin/(dashboardOptions)/user-management/manage-users/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@/components/reusable/Buttons'
-import { Plus } from 'lucide-react'
-import React from 'react'
+import { Button } from "@/components/reusable/Buttons";
+import { Plus, UserRound } from "lucide-react";
+import React from "react";
 
 const ManageUsers = () => {
   return (
     <div className="">
-      <div>
-        <div className="flex justify-between items-center pt-5 pb-4 px-4 md:px-8">
+      <div className="px-[3%]">
+        <div className="flex justify-between items-center pt-5 pb-4 ">
           <p className="text-sm md:text-2xl font-[500]">User Management</p>
           <Button
             variant="dark"
@@ -16,8 +16,34 @@ const ManageUsers = () => {
           </Button>
         </div>
       </div>
+      <div className="bg-[#F0F2F5] py-10">
+        {/* User Cards */}
+        <div className="px-[3%] h-fit grid grid-cols-[auto_auto_auto] gap-x-4 items-center overflow-x-auto no-scrollbar">
+          <div className="rounded-[8px] px-[24px] py-[16px] bg-white min-w-[320px]">
+            <UserRound className="text-[#B1924E] mb-3" />
+            <p className="font-light mb-1 text-xs md:text-base text-[#585978]">
+              Total Token Holder
+            </p>
+            <p className="text-[#13142D] font-semibold text-[20px] ">239</p>
+          </div>
+          <div className="rounded-[8px] px-[24px] py-[16px] bg-white min-w-[320px] ">
+            <UserRound className="text-[#B1924E] mb-3" />
+            <p className="font-light mb-1 text-xs md:text-base text-[#585978]">
+              Token Issuer
+            </p>
+            <p className="text-[#13142D] font-semibold text-[20px] ">3</p>
+          </div>
+          <div className="rounded-[8px] px-[24px] py-[16px] bg-white min-w-[320px] ">
+            <UserRound className="text-[#B1924E] mb-3" />
+            <p className="font-light mb-1 text-xs md:text-base text-[#585978]">
+              Dealer
+            </p>
+            <p className="text-[#13142D] font-semibold text-[20px] ">20</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ManageUsers
+export default ManageUsers;
